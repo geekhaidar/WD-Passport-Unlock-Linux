7a13f69fc8b17a603ba32dcf360d2116e5f70223
Lamgea <qwrry1617@gmail.com>
2022-11-26 04:28:23 -0500

    Add convenient script

---
+++ chgpw.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+if [ -z "$1" ]; then
+	echo "please supply target device" >&2
+	exit 1
+fi
+if [ -z "$2" ]; then
+	echo "please supply a password" >&2
+	exit 1
+fi
+if [ -z "$3" ]; then
+	echo "please supply a password you want to change to" >&2
+	exit 1
+fi
+python cookpw.py $2 --change $3 | sudo sg_raw -s 72 $1 c1 e2 00 00 00 00 00 00 48 00
