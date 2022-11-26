17bc05873d5f87d1ccc8b8676167f32d8e1bb2db
Lamgea <qwrry1617@gmail.com>
2022-11-26 04:24:04 -0500

    Add password changing function, rename 'hdparm'

--- cookpw.py
+++ cookpw.py
@@ -1,71 +1,83 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
 import sys
 import argparse
 import codecs
 
 def wdc(password):
-    password = "WDC." + password
-    password = password.encode("utf-16")[2:]
-    from hashlib import sha256
-    for _ in range(1000):
-        password = sha256(password).digest()
-    return password
+	password = "WDC." + password
+	password = password.encode("utf-16")[2:]
+	from hashlib import sha256
+	for _ in range(1000):
+		password = sha256(password).digest()
+	return password
 
-def hdparm(password):
-    if password == 'NULL':
-        password = ''
-    return bytes(password.ljust(32, '\0').encode('ascii'))
+def raw(password):
+	if password == 'NULL':
+		password = ''
+	return bytes(password.ljust(32, '\0').encode('ascii'))
 
-def generate_password(password, cook_method, cmd):
-    password = cook_method(password)
+def generate_password(password, cook_method, cmd, changepw):
+	password = cook_method(password)
 
-    if cmd == 'unlock':
-        field = b'00'
-    else:
-        password = password + password
-        if cmd == 'unset':
-            field = b'10'
-        if cmd == 'set':
-            field = b'01'
+	if cmd == 'unlock':
+		field = b'00'
+	elif cmd == 'change':
+		field = b'00'
+		password = password + cook_method(changepw)
+	elif cmd == 'unset':
+		field = b'10'
+		password = password + b'\00' * 32
+	elif cmd == 'set':
+		field = b'01'
+		password = b'\00' * 32 + password
 
-    header = b'450000' + field + b'00000020'
-    header = codecs.getdecoder('hex')(header)[0]
+	header = b'450000' + field + b'00000020'
+	header = codecs.getdecoder('hex')(header)[0]
 
-    return header + password
+	return header + password
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("passwd", type=str)
-    parser.add_argument("--hdparm", action="store_true")
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument("--unset", action="store_true")
-    group.add_argument("--set", action="store_true")
+	parser = argparse.ArgumentParser()
+	parser.add_argument("passwd", type=str)
+	parser.add_argument("--raw", action="store_true",
+		help="use raw ascii input, length must be under 32, use 'NULL' for empty input")
+	group = parser.add_mutually_exclusive_group()
+	group.add_argument("--unset", action="store_true",
+		help="set the password")
+	group.add_argument("--set", action="store_true",
+		help="unset the password")
+	group.add_argument("--change", action="store", type=str, metavar="topw",
+		help="change the password")
 
-    args = parser.parse_args()
+	args = parser.parse_args()
 
-    if args.hdparm:
-        if len(args.passwd) > 32:
-            sys.exit('Password length cannot be larger than 32!')
-        method = hdparm
-    else:
-        method = wdc
+	if args.raw:
+		if len(args.raw) > 32:
+			sys.exit('Password length cannot be larger than 32!')
+		method = raw
+	else:
+		method = wdc
 
-    if args.unset:
-        # sg_raw -s 72 -i OUTPUT_FILE DEVICE c1 e2 00 00 00 00 00 00 48 00
-        cmd = 'unset'
-    elif args.set:
-        # sg_raw -s 72 -i OUTPUT_FILE DEVICE c1 e2 00 00 00 00 00 00 48 00
-        cmd = 'set'
-    else:
-        # sg_raw -s 40 -i OUTPUT_FILE DEVICE c1 e1 00 00 00 00 00 00 28 00
-        cmd = 'unlock'
+	changepw = None
+	if args.unset:
+		# sg_raw -s 72 -i OUTPUT_FILE DEVICE c1 e2 00 00 00 00 00 00 48 00
+		cmd = 'unset'
+	elif args.set:
+		# sg_raw -s 72 -i OUTPUT_FILE DEVICE c1 e2 00 00 00 00 00 00 48 00
+		cmd = 'set'
+	elif args.change:
+		# sg_raw -s 72 -i OUTPUT_FILE DEVICE c1 e2 00 00 00 00 00 00 48 00
+		cmd = 'change'
+		changepw = args.change
+	else:
+		# sg_raw -s 40 -i OUTPUT_FILE DEVICE c1 e1 00 00 00 00 00 00 28 00
+		cmd = 'unlock'
 
-    password = generate_password(args.passwd, method, cmd)
+	password = generate_password(args.passwd, method, cmd, changepw)
 
-    out = getattr(sys.stdout, 'buffer', sys.stdout)
-    out.write(password)
+	out = getattr(sys.stdout, 'buffer', sys.stdout)
+	out.write(password)
 
 if __name__ == '__main__':
-    main()
+	main()
